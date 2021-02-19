c9485304de17e0c6b2622ea4c8836d41e99a27a8
one92tb <one92tb@gmail.com>
2021-02-19 15:19:15 +0100

    add missing padding to intro bodyText

--- src/components/organisms/Jumbotron/index.js
+++ src/components/organisms/Jumbotron/index.js
@@ -1,19 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Title from '../../atoms/Title/index';
 import Subtitle, { SubtitleSize, SubtitleColor } from '../../atoms/Subtitle/index';
 import Button, { ButtonSize, ButtonTheme } from '../../atoms/Button/index';
+import BodyText, { BodyTextTheme, BodyTextSize } from '../../atoms/BodyText/index';
 import './style.css';
 
-const Jumbotron = () => (
-  <div className="jumbotron">
-    <Title>
-      it&acute;s time
-      <br />
-      for hiking
-    </Title>
-    <Subtitle size={SubtitleSize.INTRO} color={SubtitleColor.DARK_1}>loremipsum dolor</Subtitle>
-    <Button size={ButtonSize.SMALL} theme={ButtonTheme.LIGHT}>read more</Button>
-  </div>
-);
+const Jumbotron = (props) => {
+  const { text } = props;
+  return (
+    <div className="jumbotron">
+      <Title>
+        it&acute;s time
+        <br />
+        for hiking
+      </Title>
+      <Subtitle size={SubtitleSize.INTRO} color={SubtitleColor.DARK_1}>loremipsum dolor</Subtitle>
+      <Button size={ButtonSize.SMALL} theme={ButtonTheme.LIGHT}>read more</Button>
+      <BodyText
+        bodyTextTheme={BodyTextTheme.INTRO}
+        bodyTextSize={BodyTextSize.INTO}
+      >
+        {text}
+      </BodyText>
+    </div>
+  );
+};
+
+Jumbotron.defaultProps = {
+  text: 'lorem ipsum dolor',
+};
+
+Jumbotron.propTypes = {
+  text: PropTypes.string,
+};
 
 export default Jumbotron;
--- src/components/templates/Intro/index.js
+++ src/components/templates/Intro/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../../organisms/Header/index';
 import Jumbotron from '../../organisms/Jumbotron/index';
 
-const IntroTemplate = () => (
-  <section>
-    <Header />
-    <Jumbotron />
-  </section>
-);
+const IntroTemplate = (props) => {
+  const { bodyText } = props;
+  return (
+    <section>
+      <Header />
+      <Jumbotron text={bodyText} />
+    </section>
+  );
+};
+
+IntroTemplate.defaultProps = {
+  bodyText: 'lorem ipsum dolor',
+};
+
+IntroTemplate.propTypes = {
+  bodyText: PropTypes.string,
+};
 
 export default IntroTemplate;
