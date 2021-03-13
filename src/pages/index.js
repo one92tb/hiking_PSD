9e2ab635728101c1ac4c0b4c625941adb937e17d
one92tb <one92tb@gmail.com>
2021-03-13 10:58:36 +0100

    change version app from 1.0.0 to 1.0.1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import React, { useRef } from 'react';
-import Layout from '../components/layout/layout';
+import Layout from '../components/Layout/layout';
+import Description from './Description/index';
+import Explore from './Explore/index';
+import Footer from './Footer/index';
 import Intro from './Intro/index';
 import LetsGo from './LetsGo/index';
 import Offer from './Offer/index';
-import Description from './Description/index';
-import Explore from './Explore/index';
 import Quotation from './Quotation/index';
 import Subscribe from './Subscribe/index';
-import Footer from './Footer/index';
 
 const IndexPage = () => {
   const refs = {
